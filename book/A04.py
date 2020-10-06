e0c1b4c4badae9d9d052b711a5c814fd9afee84f
yuhwanwoo <6514687@naver.com>
2020-10-06 11:33:46 +0900

    max num;

--- book/A04.py
+++ book/A04.py
@@ -3,10 +3,12 @@ data=list(map(int,input().split()))
 data.sort()
 
 target=1
-for x in data:
+for x in data: # 1 1 3
     # 만들 수 없는 금액을 찾았을 때 반복 종료
+    #print(target)
     if target<x:
         break
-    target+=x
+    target=target+x
+
 
 print(target)
