22c641861103fb800003844a99808bfa96e1d474
yuhwanwoo <6514687@naver.com>
2020-10-11 14:00:03 +0900

    only num;

---
+++ programmers/lev1/12906.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    answer = []
+    answer.append(arr[0])
+    for i in range(1,len(arr)):
+        if arr[i-1]!=arr[i]:
+            answer.append(arr[i])
+        
+    print(answer)
+    return answer
+
+arr=[1,1,3,3,0,1,1]
+solution(arr)
