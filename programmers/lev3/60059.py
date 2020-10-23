eb2db9b1bb19c37979dff128d858e095e5d64659
yuhwanwoo <6514687@naver.com>
2020-10-23 15:08:56 +0900

    lock and key

--- programmers/lev3/60059.py
+++ programmers/lev3/60059.py
@@ -1,47 +1,19 @@
-# 책 답을 봤어
-def rotate_a_matrix_by_90_degree(a):
-    n=len(a)
-    m=len(a[0])
-    result=[[0]*n for _ in range(m)]
-    for i in range(n):
-        for j in range(m):
-            result[j][n-i-1]=a[i][j]
-    return result
+def rotation_matrix(key):
 
-def check(new_lock):
-    lock_length=len(new_lock)//3
-    for i in range(lock_length,lock_length*2):
-        for j in range(lock_length,lock_length*2):
-            if new_lock[i][j]!=1:
-                return False
-    return True
 
 def solution(key, lock):
-    n=len(lock)
-    m=len(key)
+    answer = True
+    extention_map=[[0]*(len(lock)*3) for _ in range(len(lock)*3)]
+    
+    for i in range(len(lock)):
+        for j in range(len(lock)):
+            extention_map[len(lock)+i][len(lock)+j]=lock[i][j]
 
-    new_lock=[[0]*(n*3) for _ in range(n*3)]
 
-    for i in range(n):
-        for j in range(n):
-            new_lock[i+n][j+n]=lock[i][j]
+    
 
-    for rotation in range(4):
-        key=rotate_a_matrix_by_90_degree(key)
-        for x in range(n*2):
-            for y in range(n*2):
-                # 자물쇠에 열쇠를 끼워 넣기
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x+i][y+j]+=key[i][j]
-                if check(new_lock)==True:
-                    return True
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x+i][y+j]-=key[i][j]
-    return False
+    return answer
 
 key=[[0, 0, 0], [1, 0, 0], [0, 1, 1]]
 lock=[[1, 1, 1], [1, 1, 0], [1, 0, 1]]
-
-print(solution(key,lock))
+solution(key,lock)
