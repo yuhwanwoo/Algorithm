210d4fff2c01512294438e6390724bb13648eb35
yuhwanwoo <6514687@naver.com>
2020-10-27 23:11:04 +0900

    self_snake

--- book/self01/A11self.py
+++ book/self01/A11self.py
@@ -12,5 +12,23 @@ time=0
 dx=[1,0,-1,0]
 dy=[0,-1,0,1]
 
-def rot_direction():
+def rot_direction(direction,c):
+
     return 0
+
+time=0
+index=0
+x,y=1,1
+direction=0
+q=[(x,y)]
+while True:
+
+    nx=x+dx[direction]
+    ny=y+dy[direction]
+    
+    if nx>=1 and nx<=n and ny>=1 and ny<=n and dummy[nx][ny]!=2:
+        if dummy[nx][ny]==0:
+
+    else:
+        time+=1
+        break
