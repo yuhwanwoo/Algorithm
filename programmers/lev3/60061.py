12ac65f40e89c7963fa8d922fa43579a90598bbd
yuhwanwoo <6514687@naver.com>
2020-11-01 19:01:57 +0900

    n/a

--- programmers/lev3/60061.py
+++ programmers/lev3/60061.py
@@ -1,5 +1,17 @@
 def pos(result):
-    
+    for x,y,a in result:
+        # 기둥일 떄
+        if a==0:
+            if y==0 or [x-1,y,1] in result or [x,y,1] in result or [x,y-1,0] in result:
+                continue
+            return False
+        # 보일 때
+        elif a==1:
+            if [x,y-1,0] in result or [x+1,y-1,0] in result or ([x-1,y,1] in result and [x+1,y,1] in result):
+                continue
+            return False
+    return True
+
 
 def solution(n,build_frame):
     data_map=[]
@@ -8,6 +20,13 @@ def solution(n,build_frame):
         x,y,a,b=structure
         if b==1:
             result.append([x,y,a])
+            if not pos(result):
+                result.remove([x,y,a])
+        if b==0:
+            result.remove([x,y,a])
+            if not pos(result):
+                result.append([x,y,a])
+    return sorted(result)
 
 n=5
 build_frame=[[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]
