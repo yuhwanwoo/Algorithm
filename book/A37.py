93df74973b659c4116a5083323016420f43467b2
yuhwanwoo <6514687@naver.com>
2020-11-18 01:12:18 +0900

    exact ranking

--- book/A37.py
+++ book/A37.py
@@ -7,3 +7,28 @@ m=int(input())
 graph=[[INF]*(n+1) for _ in range(n+1)]
 
 # 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+for i in range(1,n+1):
+    for b in range(1,n+1):
+        if a==b:
+            graph[a][b]=0
+
+for _ in range(m):
+    a,b,c=map(int,input().split())
+    # 가장 짧은 간선 정보만 저장
+    if c<graph[a][b]:
+        graph[a][b]=c
+
+for i in range(1,n+1):
+    for a in range(1,n+1):
+        for b in range(1,n+1):
+            graph[a][b]=min(graph[a][b],graph[a][k]+graph[k][b])
+
+# 수행된 결과를 출력
+for a in range(1,n+1):
+    for b in range(1,n+1):
+        # 도달할 수 없는 경우, 0을 출력
+        if graph[a][b]==INF:
+            print(0,end=" ")
+        else:
+            print(graph[a][b],end=" ")
+    print()
