5f0eeb2cc2eebcbffe83187e5c1de3a0e2ba4c6c
yuhwanwoo <6514687@naver.com>
2020-12-19 12:03:10 +0900

    12949

---
+++ programmers/lev2/12949.py
@@ -0,0 +1,11 @@
+def solution(arr1, arr2):
+    answer = [[0]*len(arr2[0]) for _ in range(len(arr1))]
+    
+    for i in range(len(arr1)):
+        for j in range(len(arr2)):
+
+    return answer
+
+arr1=[[1, 4], [3, 2], [4, 1]]
+arr2=[[3, 3], [3, 3]]
+solution(arr1,arr2)
