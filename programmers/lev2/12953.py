5e02bafe57a0b2781fc0be500304afd36580d46a
yuhwanwoo <6514687@naver.com>
2020-12-24 10:16:37 +0900

    modify min mul

--- programmers/lev2/12953.py
+++ programmers/lev2/12953.py
@@ -1,20 +1,32 @@
 def solution(arr):
     answer = 0
     mul=1
+    i=2
     arr.sort()
-    for i in range(arr[0],arr[-1]):
-        cnt=0
-        for j in range(len(arr)):
-            if arr[j]%i==0:
-                arr[j]=arr[j]//i
-                cnt+=1
-        if cnt>0:
-            mul*=i
-    
+    max_num=arr[-1]
+    if len(arr)>1:
+        while i<max_num+1:
+            print(i)
+            cnt=0
+            for j in range(len(arr)):
+                if arr[j]%i==0:
+                    cnt+=1
+            if cnt>1:
+                for j in range(len(arr)):
+                    if arr[j]%i==0:
+                        arr[j]=arr[j]//i
+                mul*=i
+                i=2
+            else:
+                
+                i+=1
+            
+    print(arr,mul)
     for i in arr:
         mul*=i
     answer=mul
+    print("답",answer)
     return answer
 
-arr=[1,2,7]
+arr=[3,4,9,16]
 solution(arr)
