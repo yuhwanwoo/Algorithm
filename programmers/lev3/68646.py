ad2253152012ed6e3b43ee1584c07d6845fda635
yuhwanwoo <6514687@naver.com>
2021-01-01 19:10:05 +0900

    ing

--- programmers/lev3/68646.py
+++ programmers/lev3/68646.py
@@ -1,24 +1,3 @@
-def solution1(a):
-    answer = 0
-    if len(a)==3:
-        return 3
-    if len(a)<=2:
-        return len(a)
-
-    for i in range(len(a)):
-        if i==0 or i==len(a)-1:
-            answer+=1
-        else:
-            front_arr=a[:i]
-            end_arr=a[i+1:]
-            front_min=min(front_arr)
-            end_min=min(end_arr)
-            if(front_min<a[i] & end_min<a[i]):
-                continue
-            else:
-                answer+=1
-    return answer
-
 def solution(a):
     answer=2
     if 0<=len(a)<=2:
@@ -37,7 +16,3 @@ def solution(a):
         return answer-1
     else:
         return answer
-
-a=[9,-1,-5]
-a=[-16,27,65,-2,58,-92,-71,-68,-61,-33]
-solution(a)
