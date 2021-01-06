348beaff1a177872d14bfa8d7608e851ad351b23
yuhwanwoo <6514687@naver.com>
2021-01-07 00:10:05 +0900

    col and bo 2nd comp

--- programmers/lev3/60061_self02.py
+++ programmers/lev3/60061_self02.py
@@ -1,6 +1,14 @@
 def judge(answer):
-    
-    return False
+    for x,y,con in answer:
+        if con==0:
+            if y==0 or [x-1,y,1] in answer or [x,y,1] in answer or [x,y-1,0] in answer:
+                continue
+            return False
+        if con==1:
+            if [x,y-1,0] in answer or [x+1,y-1,0] in answer or ([x-1,y,1] in answer and [x+1,y,1] in answer):
+                continue
+            return False
+    return True
 
 def solution(n,build_frame):
     answer=[]
@@ -8,7 +16,18 @@ def solution(n,build_frame):
         x,y,con,jud=frame
         if jud==1:
             answer.append([x,y,con])
-
+            print(judge(answer))
+            if judge(answer):
+                continue
+            else:
+                answer.pop()
+        if jud==0:
+            answer.remove([x,y,con])
+            if judge(answer):
+                continue
+            else:
+                answer.append([x,y,con])
+    answer.sort()
     return answer
 
 
