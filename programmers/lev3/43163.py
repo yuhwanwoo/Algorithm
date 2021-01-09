274ad89a9099cafffb5a6e74cc956cbd0afd8922
yuhwanwoo <6514687@naver.com>
2021-01-09 13:46:00 +0900

    word conver

---
+++ programmers/lev3/43163.py
@@ -0,0 +1,11 @@
+def solution(begin, target, words):
+    answer = 0
+
+
+
+    return answer
+
+begin="hit"
+target="cog"
+words=["hot", "dot", "dog", "lot", "log", "cog"]
+solution(begin,target,words)
--- programmers/lev3/test.py
+++ programmers/lev3/test.py
@@ -1,9 +1,48 @@
-def solution(a):
-    answer=0
-    a.sort()
-    print(a)
+answer = 0
+def dfs(begin,target,words,visited):
+    global answer
+    stacks = [begin]
+ 
+    while stacks:
+        print(stacks,"스택")
+        # 스택을 활용한 dfs 구현
+        stack = stacks.pop()
+        print("stack",stack)
+        if stack == target:
+            return answer
+        
+        for w in range(0,len(words)):
+            print([i for i in range(0,len(words[w])) if words[w][i]!=stack[i]], words[w])
+            # 조건 1. 한 개의 알파벳만 다른 경우
+            if len([i for i in range(0,len(words[w])) if words[w][i]!=stack[i]]) == 1:
+                print(words[w])
+                if visited[w] != 0:
+                    continue
+ 
+                visited[w] = 1
+ 
+                # stack에 추가
+                stacks.append(words[w])
+            
+        
+        # depth +
+        answer +=1
+        print("answer",answer)
+def solution(begin, target, words):
+    global answer
+    if target not in words:
+        return 0
+    
+    visited = [0 for i in words]
+    
+    dfs(begin,target,words,visited)
+    print(answer)
     return answer
 
-a=[9,-1,-5]
-a=[-16,27,65,-2,58,-92,-71,-68,-61,-33]
-solution(a)
+
+begin="hit"
+target="cog"
+words=["hot", "dog", "lot", "dot", "log", "cog","loj"]
+solution(begin,target,words)
+
+
