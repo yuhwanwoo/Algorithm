b7fde0703b21f5909c06dd247cee1f12c75e5f61
yuhwanwoo <6514687@naver.com>
2021-02-11 19:14:05 +0900

    ans 12924

--- programmers/lev3/12924.py
+++ programmers/lev3/12924.py
@@ -1,17 +1,32 @@
-def solution(n):
+import math
+
+# 효율성 실패
+def solution1(n):
     answer = 0
-    min_num=0
-    for i in range(n):
-        if n<=sum(range(i+1)):
-            min_num=i
-            break
-    print(min_num)
-    for i in range(min_num,n+1):
-        for j in range(n+1):
+    
+    num=math.ceil(n/2)
+    
+    for i in range(1,num+2):
+        for j in range(i+1):
+            
             if (i*(i+1)//2)-(j*(j+1)//2)==n:
                 answer+=1
+                break
+    answer+=1
     print(answer)
     return answer
 
-n=15
+def solution(n):
+    answer = 1
+    for i in range(1, int(n/2) + 1):
+        s = 0
+        while s < n :
+            s += i
+            i += 1
+        if s == n :
+            answer += 1
+
+    return answer
+
+n=5000
 solution(n)
