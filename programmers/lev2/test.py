cc8a6eee2ae0b0f03f75474f28390ff333bf3710
yuhwanwoo <6514687@naver.com>
2021-02-13 15:46:03 +0900

    lv2 수식최대화

--- programmers/lev2/test.py
+++ programmers/lev2/test.py
@@ -1,8 +1,2 @@
-phoneBook=["119","345","1192314","3456"]
-
-phoneBook=sorted(phoneBook)
-print(phoneBook)
-print(phoneBook[1:])
-print(list(zip(phoneBook,phoneBook[1:])))
-for p1, p2 in zip(phoneBook,phoneBook[1:]):
-    print(p1,p2)
+n="300"
+print(eval(n))
