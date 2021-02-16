6cc4d0b79154d12467e2accac5777d57920ca0a3
yuhwanwoo <6514687@naver.com>
2021-02-16 23:55:55 +0900

    lv1 실패율 성공

--- programmers/lev1/42889.py
+++ programmers/lev1/42889.py
@@ -8,16 +8,25 @@ def solution(N, stages):
     print(people)
     minus=0
     for i in range(1,len(people)-1):
+        if size-minus<=0:
+            temp.append((0,i))
+            continue
         temp.append((people[i]/(size-minus),i))
         size=size-minus
         minus=people[i]
+        
     print(temp)
     temp.sort(key=lambda x:(-x[0],x[1]))
     print(temp)
     for i in temp:
         answer.append(i[1])
+    if len(temp)<N:
+        temp+=([0]*(N-len(answer)))
+    print(answer)
     return answer
 
 N=5
 stages=[2, 1, 2, 6, 2, 4, 3, 3]
+N=3
+stages=[2,2,2,2,2]
 solution(N,stages)
