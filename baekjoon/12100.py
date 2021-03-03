e2a1d1092e5011ee8ca240d5bba03b11b8cb0fb8
yuhwanwoo <6514687@naver.com>
2021-03-03 22:07:19 +0900

    2xn 타일링

---
+++ baekjoon/12100.py
@@ -0,0 +1,9 @@
+n=int(input())
+
+
+arr=[]
+
+for _ in range(n):
+    arr.append(input().split())
+
+print(arr)
---
+++ programmers/lev3/12900.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    a=[0]*(n+2)
+    a[0]=1
+    a[1]=2
+    for i in range(2,n+2):
+        a[i]=a[i-1]+a[i-2]
+    print(a)
+    return a[n-1]
+
+n=7
+print(solution(n))
