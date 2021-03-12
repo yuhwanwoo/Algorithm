1ef085db4af98fa8335ababb3d83523a0a12bddc
yuhwanwoo <6514687@naver.com>
2021-03-12 20:52:16 +0900

    11052 answer

---
+++ baekjoon/11052_answer.py
@@ -0,0 +1,14 @@
+# https://mhko411.tistory.com/64
+
+n=int(input())
+
+dp=[0]*1001
+cardpack=[0]
+
+cardpack+=(list(map(int,input().split())))+[0]*10
+
+for i in range(1,n+1):
+    for j in range(0,i):
+        dp[i]=max(dp[i],dp[j]+cardpack[i-j])
+
+print(dp[n])
