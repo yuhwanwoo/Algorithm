150bc82628e452680810330af0ff17ba62f6aa11
yuhwanwoo <6514687@naver.com>
2021-03-15 00:11:13 +0900

    바뀐것 없음

--- baekjoon/11052_answer.py
+++ baekjoon/11052_answer.py
@@ -3,9 +3,10 @@
 n=int(input())
 
 dp=[0]*1001
+
 cardpack=[0]
 
-cardpack+=(list(map(int,input().split())))+[0]*10
+cardpack += (list(map(int,input().split())))+[0]*1001
 
 for i in range(1,n+1):
     for j in range(0,i):
