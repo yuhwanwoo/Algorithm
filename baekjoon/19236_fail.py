f6e2d8628f124ed4b3eb0fb607672a5d8e79a508
yuhwanwoo <6514687@naver.com>
2021-04-04 22:27:04 +0900

    shark fail

--- baekjoon/19236_fail.py
+++ baekjoon/19236_fail.py
@@ -23,20 +23,33 @@ print(fish)
 
 # 물고기 16마리를 돌려
 def rot_fish(fish_map):
-    for i in range(1,17):
+    fish.keys().sort()
+    for i in fish.keys():
         fx,fy=fish_map[i]
         for i in range(8):
-            nx=fx+dire_map[(fx+i)%8]
-            ny=fx+dire_map[(fy+i)%8]
+            nx=fx+dx[dire_map[fx]-1+i]
+            ny=fy+dy[dire_map[fy]-1+i]
             if fish_map[nx][ny] != 18 and nx>0 and ny<4 and ny>0 and ny<4:
-                fish_map[nx][ny]=i
+                # 원래 내값
+                temp_x,temp_y=fx,fy
+                temp=fish_map[fx][fy]
+                temp_dir=dire_map[fx][fy]
+                # 원래 자리에 있던애 내 자리로
+                
+                fish_map[fx][fy]=fish_map[nx][ny]
+                dire_map[fx][fy]=dire_map[nx][ny]
+                fish_map[nx][ny]=temp
+                dire_map[nx][ny]=temp_dir
+
                 
                 break
 
 def solution(x,y,num):
     if x<0 or y<0 or x>3 or y>3:
         return num
-    fish_map[x][y]=18
+    eaten=fish_map[x][y]
+    del fish[eaten]
+    fish_map[x][y]
     return 0
 
 solution(0,0,fish_map[0][0])
