d65ae1693dcbaf84cd7de02a605eb54875bb99a6
yuhwanwoo <6514687@naver.com>
2021-04-10 13:52:41 +0900

    단어변환

--- test/nemo.py
+++ test/nemo.py
@@ -1,13 +1,15 @@
+import copy
+
 leng,totalSum,cnt=0,0,0
 n,m=map(int,input().split())
-mapp=[[0]*m  for _ in range(n) ]
+mapp=[[2]*m  for _ in range(n) ]
 
 #print(mapp)
 leng=n*m
 cnt=0
 
 def dfs(index,num):
-    global cnt
+    global cnt,mapp
     if leng==index:
         return
     
@@ -15,29 +17,31 @@ def dfs(index,num):
     c=index%m
     mapp[r][c]=num
     
-    # print(mapp)
-    
-    if(mapp[r][c]==1):
-        # mapp[r][c]=2
-        # print(mapp)
-        # mapp[r][c]=1
-        if(r<n or r-1>=0 or c<m or c-1>=0):
-            for i in range(1,n):
-                judge=False
-                for j in range(1,m):
-                    if mapp[i][j-1]==1 and mapp[i-1][j]==1 and mapp[i-1][j-1]==1 and mapp[i][j]==1:
-                        cnt+=1
-                        judge=True
-                        break
-                if judge:
+    print(mapp)
+
+
+    if(r<n or r-1>=0 or c<m or c-1>=0):
+        for i in range(1,n):
+            judge=False
+            for j in range(1,m):
+                if mapp[i][j-1]==1 and mapp[i-1][j]==1 and mapp[i-1][j-1]==1 and mapp[i][j]==1:
+                    cnt+=1
+                    judge=True
                     break
+            if judge:
+                break
 
 
-    
+    temp=copy.deepcopy(mapp)    
     dfs(index+1,1)
+    mapp=copy.deepcopy(temp)    
     dfs(index+1,0)
 
+
+temp=copy.deepcopy(mapp)
 dfs(0,1)
+mapp=copy.deepcopy(temp)
 dfs(0,0)
 totalSum=1<<leng
+print(cnt)
 print(totalSum-cnt)
---
+++ test/p43163.py
@@ -0,0 +1,36 @@
+INF=int(1e9)
+ans=INF
+def dfs(begin,target,words,visited,answer):
+    
+    if begin==target:
+        global ans
+        ans=min(ans,answer)
+        return answer
+
+    for i in range(len(words)):
+        if visited[i]==1:
+            continue
+        cnt=0
+        for j in range(len(words[i])):
+            if words[i][j]!=begin[j]:
+                cnt+=1
+            if cnt>1:
+                break
+        if cnt==1:
+            visited[i]=1
+            dfs(words[i],target,words,visited,answer+1)
+            visited[i]=0
+
+    return INF
+
+def solution(begin, target, words):
+    answer = 0
+    global ans
+    n=0
+    min_num=INF
+    visited=[0 for _ in range(len(words))]
+    
+    dfs(begin,target,words,visited,0)
+    if ans==INF:
+        return 0
+    return ans
