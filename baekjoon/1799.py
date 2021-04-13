a86ad96ad5259094d3d7246a5979530053ebfa1b
yuhwanwoo <6514687@naver.com>
2021-04-14 00:46:56 +0900

    백준 비숍 완료

--- baekjoon/1799.py
+++ baekjoon/1799.py
@@ -1,36 +1,53 @@
 n=int(input())
 
 chess_map=[]
-bishop=[]
+black=[]
+white=[]
+color=[[0]*n for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        color[i][j]=(i % 2 == 0 and j % 2 == 0) or (i % 2 != 0 and j % 2 != 0)
 
 for i in range(n):
     chess_map.append(list(map(int, input().split())))
     for j in range(n):
-        if chess_map[i][j]==1:
-            bishop.append((i,j))
-# print(bishop)
-leng=len(bishop)
-cnt=0
+        # True가 검은색
+        if chess_map[i][j]==1 and color[i][j]==1:
+            black.append((i,j))
+        # False가 흰색
+        if chess_map[i][j]==1 and color[i][j]==0:
+            white.append((i,j))
+
+Bcnt=0
+Wcnt=0
 
 isused01=[0]*(n*2-1)
 isused02=[0]*(n*2-1)
 
-def fun(index,count):
-    global cnt
-    if index==leng:
-        cnt=max(cnt,count)
+def fun(bishop,index,count):
+    global Bcnt, Wcnt
+    if index==len(bishop):
+        rx,ry=bishop[index-1]
+        if color[rx][ry]:
+            Bcnt=max(Bcnt,count)
+        else:
+            Wcnt=max(Wcnt,count)
         return
 
     x,y=bishop[index]
     if isused01[x+y] or isused02[x-y+n-1]:
-        fun(index+1,count)
+        fun(bishop,index+1,count)
     else:
         isused01[x+y]=1
         isused02[x-y+n-1]=1
-        fun(index+1,count+1)
+        fun(bishop,index+1,count+1)
         isused01[x+y]=0
         isused02[x-y+n-1]=0
-        fun(index+1,count)
+        fun(bishop,index+1,count)
 
-fun(0,0)
-print(cnt)
+if len(black)>0:
+    fun(black,0,0)
+if len(white)>0:
+    fun(white,0,0)
+print(Bcnt+Wcnt)
--- baekjoon/test.py
+++ baekjoon/test.py
@@ -1,36 +1,7 @@
-import sys
+color=[[0]*5 for _ in range(5)]
 
-class Trie:
-    def __init__(self):
-        self.root = {} # children
+for i in range(5):
+    for j in range(5):
+        color[i][j]=(i % 2 == 0 and j % 2 == 0) or (i % 2 != 0 and j % 2 != 0)
 
-    
-    def add(self, foods):
-        cur = self.root
-
-        for food in foods:
-            if food not in cur:
-                cur[food] = {} # dictonary => children
-            cur = cur[food]
-        cur[0] = True # leaf node
-
-    def travel(self, level,cur):
-        if 0 in cur:
-            return
-
-        cur_children=sorted(cur)
-
-        for child in cur_children:
-            print("--"*level+child)
-            self.travel(level+1,cur[child])
-
-N = int(sys.stdin.readline().strip())
-nest=Trie()
-for _ in range(N):
-    input_line = sys.stdin.readline().split()
-    K = input_line[0]
-    foods = input_line[1:]
-    nest.add(foods)
-nest.travel(0, nest.root)
-
-
+print(color)
