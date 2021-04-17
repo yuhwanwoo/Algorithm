1af669781b6dfe66c66fff01d8d053b4dce6d37c
yuhwanwoo <6514687@naver.com>
2021-04-17 16:46:20 +0900

    백준 쉬운계단수

--- baekjoon/10844.py
+++ baekjoon/10844.py
@@ -1,14 +1,17 @@
 n=int(input())
 
-MinNumber=0
+dp=[[0]*10 for _ in range(101)]
 
-if n%2==0:
-    MinNumber="11"*(n//2)
-else:
-    MinNumber="7"+"11"*(n//2-1)
+for i in range(1,10):
+    dp[1][i]=1
 
-# 1 2 3 4 5 6 7 8 9 0
-# 2 5 5 4 5 6 3 7 6 6
+for i in range(2,n+1):
+    for j in range(10):
+        if j==0:
+            dp[i][j]=dp[i-1][1]
+        elif j==9:
+            dp[i][j]=dp[i-1][8]
+        else:
+            dp[i][j]=dp[i-1][j-1]+dp[i-1][j+1]
 
-# 2 3 4 5 6 7 8  9  10 11 12
-# 1 7 4 2 6 8 20 18 23 20 28
+print(sum(dp[n])%1000000000)
