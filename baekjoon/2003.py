c89ed06b99c3f26a023f742757ec4d02c3fd0f2e
yuhwanwoo <6514687@naver.com>
2021-04-26 23:31:21 +0900

    수들의 합2

--- baekjoon/2003.py
+++ baekjoon/2003.py
@@ -1,1 +1,20 @@
-n,m=input().split()
+n,m=map(int,input().split())
+arr=list(map(int,input().split()))
+
+start=0
+end=0
+summ=0
+result=0
+while True:
+    if summ>=m:
+        summ-=arr[start]
+        start+=1
+    elif end==n:
+        break
+    else:
+        summ+=arr[end]
+        end+=1
+    if summ==m:
+        result+=1
+
+print(result)
