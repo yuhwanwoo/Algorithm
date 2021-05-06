0723c58affe2c5e3765d56f6a39245594a1140f3
yuhwanwoo <6514687@naver.com>
2021-05-07 00:33:38 +0900

    트리 죠지는중

--- baekjoon/2533_answer.py
+++ baekjoon/2533_answer.py
@@ -5,7 +5,8 @@ sys.setrecursionlimit(10**9)
  
 N=int(sys.stdin.readline())
 Tree=[[] for _ in range(N+1)]
-check=[0 for _ in range(N+1)]
+# check=[0 for _ in range(N+1)]
+# print(check)
 for _ in range(N-1):
     u,v=map(int,sys.stdin.readline().split())
     Tree[u].append(v)
@@ -16,16 +17,17 @@ print(DP)
 check=[True for _ in range(N+1)]
 def DFS(cur):
     check[cur]=False#방문체크
+    # print(check)
     DP[cur][0]=1#현재 노드 포함할때
     DP[cur][1]=0#현재 노드 포함하지 않을때
-    print(DP)
-    print(check)
+    # print(DP)
+    # print(check)
     for i in Tree[cur]:#연결된 노드 탐색
         if check[i]:#자식노드라면 
             DFS(i)
             DP[cur][0]+=DP[i][1]#현재노드 포함했을때
             DP[cur][1]+=max(DP[i][0],DP[i][1])#현재노드를 포함 안했을때
-            print("for안",DP)
+            # print("for안",DP)
  
 DFS(1)
 print(DP)
