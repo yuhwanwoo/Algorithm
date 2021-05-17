61916c60600c525c0b99f4a269050778db3690d9
yuhwanwoo <6514687@naver.com>
2021-05-17 18:51:10 +0900

    괄호 회전하기

--- programmers/lev2/76502.py
+++ programmers/lev2/76502.py
@@ -1,29 +1,48 @@
-# 이 풀이 아직 실패인데 해결하고 스택으로 풀자
-
 def equal_s(s):
-    big=0
-    mid=0
-    small=0
+    stack=[]
+    cnt=0
+    if len(s)==1:
+        return False
     for i in range(len(s)):
         if s[i]=='(':
-            big+=1
-        elif s[i]==')':
-            big-=1
-        elif s[i]=='[':
-            mid+=1
-        elif s[i]==']':
-            mid-=1
+            cnt+=1
+            stack.append(s[i])
         elif s[i]=='{':
-            small+=1
+            cnt+=1
+            stack.append(s[i])
+        elif s[i]=='[':
+            cnt+=1
+            stack.append(s[i])
+        elif s[i]==')':
+            cnt-=1
+            if not stack:
+                return False
+            temp=stack.pop()
+            if temp=='(':
+                continue
+            else:
+                return False
         elif s[i]=='}':
-            small-=1
+            cnt-=1
+            if not stack:
+                return False
+            temp=stack.pop()
+            if temp=='{':
+                continue
+            else:
+                return False
         else:
-            continue
-        if small<0 or mid<0 or big<0:
-            return False
-    if small==0 and mid==0 and big==0:
-        return True
-    return False
+            cnt-=1
+            if not stack:
+                return False
+            temp=stack.pop()
+            if temp=='[':
+                continue
+            else:
+                return False
+    if cnt!=0:
+        return False
+    return True
 
 def solution(s):
     answer = 0
@@ -31,12 +50,17 @@ def solution(s):
     for i in range(n):
         s=s[1:]+s[0]
         if equal_s(s):
+            print(s)
             answer+=1
 
     return answer
 
 s='[](){}'
 s="}}}"
-s="{{{{{{{"
+s="}]()[{"
 s="([{)}]"
+s="}}}}}}}"
+s="("
+s=")"
+s="){("
 print(solution(s))
