3d90959b7407b62f26d176749addfab56b8390ad
yuhwanwoo <6514687@naver.com>
2021-06-07 19:54:27 +0900

    strahler 순서

--- baekjoon/9470.py
+++ baekjoon/9470.py
@@ -3,21 +3,40 @@ from collections import deque
 t=int(input())
 for _ in range(t):
     k,m,p=map(int,input().split())
-    sted=[[] for _ in range(m+1)]
-    edst=[[] for _ in range(m+1)]
+
+    indegree=[0]*(m+1)
+    graph=[[] for _ in range(m+1)]
+
+    # 인덱스 0은 최대순서 1은 개수
+    seq=[[0,0] for _ in range(m+1)]
+
     q=deque()
     for i in range(p):
         s,e=map(int,input().split())
-        sted[s].append(e)
-        edst[e].append(s)
-    print(sted)
-    #print(edst)
-    for i in range(1,m+1):
-        if not edst[i]:
-            q.append((i,1))
-    print(q)
-    while q:
-        n,seq=q.popleft()
-        for i in sted[n]:
-            print(i)
+        graph[s].append(e)
+        indegree[e]+=1
 
+    for i in range(1,m+1):
+        if indegree[i]==0:
+            q.append(i)
+            seq[i][0]=1
+            
+
+    result=[]
+
+    while q:
+        now=q.popleft()
+        result.append(now)
+
+        for i in graph[now]:
+            indegree[i]-=1
+            if seq[i][0]<seq[now][0]:
+                seq[i][0]=seq[now][0]
+                seq[i][1]=1
+            elif seq[i][0] == seq[now][0]:
+                seq[i][1]+=1
+            if indegree[i]==0:
+                if seq[i][1]>1:
+                    seq[i][0]+=1
+                q.append(i)
+    print(k,seq[m][0])
