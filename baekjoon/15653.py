109bc5267582cd3570b70e8f2521f22df7f3b323
yuhwanwoo <6514687@naver.com>
2021-06-18 19:12:55 +0900

    구슬 실패

---
+++ baekjoon/15653.py
@@ -0,0 +1,49 @@
+from collections import deque
+
+n,m=map(int,input().split())
+
+arr=[]
+rx,ry=0,0
+bx,by=0,0
+q=deque()
+dx=[0,-1,0,1]
+dy=[-1,0,1,0]
+
+
+
+for i in range(n):
+    arr.append(list(input()))
+    for j in range(m):
+        if arr[i][j]=="B":
+            bx,by=i,j
+        if arr[i][j]=="R":
+            rx,ry=i,j
+
+
+
+def dfs(rx,ry,bx,by,cnt):
+
+    q.append((rx,ry,bx,by))
+
+    while q:
+        rx,ry,bx,by=q.popleft()
+
+        for i in range(4):
+            nrx=rx+dx[i]
+            nry=ry+dy[i]
+            nbx=bx+dx[i]
+            nby=by+dy[i]
+            if i==0:
+                while True:
+                    if ry<=by:
+                        if oversize(nrx,nry):
+                            if arr[nrx][nry]=="O":
+                                break
+                            if arr[nrx][nry]==".":
+                                rx=nrx
+                                ry=nry
+
+def oversize(x,y):
+    if x<0 or x>=n or y<0 or y>=m:
+        return False
+    return True
