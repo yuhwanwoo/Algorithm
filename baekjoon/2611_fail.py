f6ede3a45bab104240065c0945e9c76c3a191ddf
yuhwanwoo <6514687@naver.com>
2021-06-29 20:17:57 +0900

    2611

---
+++ baekjoon/2611_fail.py
@@ -0,0 +1,39 @@
+from collections import deque
+import sys
+
+input=sys.stdin.readline
+
+n=int(input())
+m=int(input())
+
+indegree=[0]*(n+1)
+graph=[[] for _ in range(n+1)]
+result=[[0,""] for _ in range(n+1)]
+
+q=deque()
+
+for i in range(m):
+    s,e,d=map(int,input().split())
+    graph[s].append((e,d))
+    indegree[e]+=1
+
+result[1][1]="1"
+q.append(1)
+
+while q:
+    now=q.popleft()
+
+    for loc in graph[now]:
+        i,cost=loc
+        indegree[i]-=1
+        if result[i][0]<result[now][0]+cost:
+            result[i][0]=result[now][0]+cost
+            result[i][1]=result[now][1]
+            result[i][1]+=str(i)
+        if indegree[i]==0:
+            if indegree[1]==0:
+                break
+            q.append(i)
+print(result)
+print(result[1][0])
+print(*list(result[1][1]))
