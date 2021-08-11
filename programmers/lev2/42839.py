64f4126a95557a2b25ca2da9a21eda9aea5bf472
yuhwanwoo <6514687@naver.com>
2021-08-11 21:29:27 +0900

    소수찾기 수정

--- programmers/lev2/42839.py
+++ programmers/lev2/42839.py
@@ -1,14 +1,13 @@
 from itertools import permutations
 
-def is_prime_not_bad(n: int) -> bool:
+def isPrime(n):
     if n < 2: 
         return False 
     if n == 2:
         return True 
     if n % 2 == 0: 
         return False
-    l = round(n ** 0.5) + 1
-    for i in range(3, l, 2):
+    for i in range(3, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
     return True
@@ -16,15 +15,14 @@ def is_prime_not_bad(n: int) -> bool:
 
 def solution(numbers):
     answer = 0
-    ans_set=[]
-    for i in range(0,len(numbers)):
-        ans_set+=list(map(''.join,(permutations(numbers,i+1))))
-    
-    ans_set=set(list(map(int,ans_set)))
-    for i in ans_set:
-        if is_prime_not_bad(i):
+    ans=[]
+    for i in range(len(numbers)):
+        ans+=list((map(int,map(''.join,(permutations(numbers,i+1))))))
+    ans=set(ans)
+    for i in ans:
+        if isPrime(i):
             answer+=1
     return answer
 
-numbers="17"
+numbers="011"
 solution(numbers)
