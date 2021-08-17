e5cc08f5f6a04b79581e74e9941dbc714d9479d4
yuhwanwoo <6514687@naver.com>
2021-08-18 00:01:28 +0900

    java init

---
+++ java/src/lv3_67259.java
@@ -0,0 +1,13 @@
+import java.util.Queue;
+
+public class lv3_67259 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution(int[][] board) {
+        int answer = Integer.MAX_VALUE;
+
+        return 0;
+    }
+}
