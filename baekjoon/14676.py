b4627835ca43198ccf42307fb7bc9157b2a95c1d
yuhwanwoo <6514687@naver.com>
2021-08-23 19:50:24 +0900

    weekly 3

--- baekjoon/14676.py
+++ baekjoon/14676.py
@@ -7,6 +7,7 @@ n, m, k = map(int, input().split())
 indegree = [0] * (n + 1)
 graph = [[] for _ in range(n+1)]
 
+visited = [0] * (n + 1)
 
 for _ in range(m):
     s, e = map(int, input().split())
@@ -14,5 +15,32 @@ for _ in range(m):
 
     indegree[e] += 1
 
+flag = False
+
 for _ in range(k):
-    s, e = map(int, input().split())
+    mode, ob = map(int, input().split())
+
+    if mode == 1:
+        if indegree[ob] != 0:
+            flag = True
+            break
+        
+        visited[ob] += 1
+        if visited[ob] == 1:
+            for e in graph[ob]:
+                indegree[e] -= 1    
+    else:
+        if visited[ob] <= 0:
+            flag = True
+            break
+        
+        visited[ob] -= 1
+        if visited[ob] == 0:
+            for e in graph[ob]:
+                indegree[e] += 1
+
+if flag:
+    print("Lier!")
+else:
+    print("King-God-Emperor")
+
---
+++ baekjoon/17616.py
@@ -0,0 +1,1 @@
+n, m, x = map(int, input().split())
