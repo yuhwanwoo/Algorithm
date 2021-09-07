cca55741b785101074fc4197207fe422378b3f69
yuhwanwoo <6514687@naver.com>
2021-09-08 00:10:16 +0900

    two pointer ~ing

--- baekjoon/2003.py
+++ baekjoon/2003.py
@@ -1,20 +1,13 @@
-n,m=map(int,input().split())
-arr=list(map(int,input().split()))
+n, m = map(int,input().split())
+arr = list(map(int,input().split()))
 
-start=0
-end=0
-summ=0
-result=0
+start = 0
+end = 0
+count = arr[0]
+
 while True:
-    if summ>=m:
-        summ-=arr[start]
-        start+=1
-    elif end==n:
-        break
-    else:
-        summ+=arr[end]
-        end+=1
-    if summ==m:
-        result+=1
+    
 
-print(result)
+    if count < m
+
+    break
---
+++ programmers/lev3/67258.py
@@ -0,0 +1,14 @@
+def solution(gems):
+    answer = []
+    start = 0
+    end = 1
+    while True:
+        
+
+        break
+    
+
+    return answer
+
+gems = ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
+solution(gems)
