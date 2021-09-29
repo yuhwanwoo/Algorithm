2591c58e78a5e93bcd9d4deab7df699d0c051241
yuhwanwoo <6514687@naver.com>
2021-09-30 00:32:43 +0900

    부분합 init

---
+++ baekjoon/2167_ans02.py
@@ -0,0 +1,17 @@
+#https://ywtechit.tistory.com/102
+
+import sys
+input = sys.stdin.readline
+ 
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+memo = [[0] * (m+1) for _ in range(n+1)]
+ 
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        memo[i][j] = arr[i-1][j-1] + memo[i][j-1] + memo[i-1][j] - memo[i-1][j-1]
+ 
+k = int(input())
+for _ in range(k):
+    i, j, x, y = map(int, input().split())
+    print(memo[x][y] - memo[i-1][y] - memo[x][j-1] + memo[i-1][j-1])
