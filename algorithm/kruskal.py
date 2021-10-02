dd5a6e9ed0e38f3c2b37363fe762980cc558fc6e
yuhwanwoo <6514687@naver.com>
2021-10-02 20:20:11 +0900

    ~ing

---
+++ algorithm/kruskal.py
@@ -0,0 +1,12 @@
+def dind_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
