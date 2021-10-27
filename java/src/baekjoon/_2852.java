d0886efc39107e776ea65e8d2904586ecc1ea52d
yuhwanwoo <6514687@naver.com>
2021-10-27 15:02:08 +0900

    nba 농구 java

--- java/src/baekjoon/_2852.java
+++ java/src/baekjoon/_2852.java
@@ -12,15 +12,14 @@ public class _2852 {
 
         int n = Integer.parseInt(br.readLine());
 
-        int[] team = new int[n];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
         int win_team;
         int min_time;
         int[] score = {0, 0};
+        int[] ans_time = {0, 0};
         Record[] records = new Record[n];
 
         for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             win_team = Integer.parseInt(st.nextToken());
             String tmp = st.nextToken();
             String[] time = tmp.split(":");
@@ -28,10 +27,52 @@ public class _2852 {
             records[i] = new Record(win_team, min_time);
         }
 
+        win_team = records[0].team;
+        score[win_team - 1] += 1;
+        min_time = records[0].time;
+
         for (int i = 1; i < n; i++) {
+            int tmp_team = records[i].team;
+            int tmp_time = records[i].time;
+            if (score[0] > score[1]) {
+                ans_time[0] += (tmp_time - min_time);
+            }
+            if (score[0] < score[1]) {
+                ans_time[1] += (tmp_time - min_time);
+            }
+            score[tmp_team - 1] += 1;
+
+            min_time = tmp_time;
 
         }
 
+        if (score[0] > score[1]) {
+            ans_time[0] += (48 * 60 - min_time);
+        }
+        if (score[0] < score[1]) {
+            ans_time[1] += (48 * 60 - min_time);
+        }
+
+        for (int i = 0; i < 2; i++) {
+            int h = ans_time[i] / 60;
+            int m = ans_time[i] % 60;
+            String hour;
+            String min;
+            if (h < 10) {
+                hour = "0" + Integer.toString(h);
+            }else {
+                hour = Integer.toString(h);
+            }
+
+            if (m < 10) {
+                min = "0" + Integer.toString(m);
+            }else {
+                min = Integer.toString(m);
+            }
+
+            System.out.println(hour + ":" + min);
+
+        }
     }
 }
 
