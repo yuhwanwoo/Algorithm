b8ac48101ade81244232ba9b567da56db0d89849
yuhwanwoo <6514687@naver.com>
2021-10-30 00:56:49 +0900

    boj 2470 완료

--- java/src/baekjoon/_2470.java
+++ java/src/baekjoon/_2470.java
@@ -22,11 +22,12 @@ public class _2470 {
         int ans2 = 0;
 
         for (int i = 0; i < n - 1; i++) {
+
             int now = arr[i];
             int target = now * -1;
-//            System.out.println("target : " + target);
+
             int tmp = binary_search(i + 1, n, target, arr, n);
-//            System.out.println(tmp);
+
             int sum = Math.abs(arr[i] + tmp);
             if (Math.abs(answer) >= sum) {
                 answer = sum;
@@ -35,15 +36,26 @@ public class _2470 {
             }
         }
 
-        System.out.println(ans1 + " " + ans2);
+        if (ans1 < ans2) {
+            System.out.println(ans1 + " " + ans2);
+        } else {
+            System.out.println(ans2 + " " + ans1);
+        }
+
 
     }
 
     public static int binary_search(int start, int end, int target, int[] arr, int n) {
-        int mid = 0;
-        while (start < end) {
-            mid = (start + end) / 2;
+        int min  = Integer.MAX_VALUE;
+        int answer = 0;
 
+        while (start < end) {
+            int mid = (start + end) / 2;
+
+            if (Math.abs(target - arr[mid]) < min) {
+                min = Math.abs(target - arr[mid]);
+                answer = arr[mid];
+            }
             if (arr[mid] < target) {
                 start = mid + 1;
             } else if (arr[mid] > target) {
@@ -52,15 +64,7 @@ public class _2470 {
                 return target;
             }
         }
-        System.out.println(mid);
-        if (mid == n - 1) {
-            return arr[mid];
-        }
 
-        if (Math.abs(arr[mid] - target) <= Math.abs(arr[mid + 1] - target)) {
-            return arr[mid];
-        } else {
-            return arr[mid + 1];
-        }
+        return answer;
     }
 }
