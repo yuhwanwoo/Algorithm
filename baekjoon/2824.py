5e0744e7082bd0fc5b7a119701e65038188c2165
yuhwanwoo <6514687@naver.com>
2021-11-10 18:29:42 +0900

    boj 2824

--- baekjoon/2824.py
+++ baekjoon/2824.py
@@ -1,7 +1,34 @@
+# 유클리드 호제법
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
+
 n = int(input())
 
-arrA = list(map(int, input().split()))
+a = list(map(int, input().split()))
+
+mulA = 1
+
+for i in a:
+    mulA *= i
 
 m = int(input())
+b = list(map(int, input().split()))
 
-arrB = list(map(int, input().split()))
+mulB = 1
+
+for i in b:
+    mulB *= i
+
+def gcd(x, y):
+
+    if y == 0:
+        return x
+    
+    return gcd(y, x%y)
+
+if mulA > mulB:
+    print(str(gcd(mulA, mulB))[-9:])
+else:
+    print(str(gcd(mulB, mulA))[-9:])
