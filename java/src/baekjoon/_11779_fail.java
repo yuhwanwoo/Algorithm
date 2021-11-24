c5961e207f81d1a2909df5d4d157c26ffbdefd3a
yuhwanwoo <6514687@naver.com>
2021-11-24 21:51:38 +0900

    11779 fail

---
+++ java/src/baekjoon/_11779_fail.java
@@ -0,0 +1,115 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class _11779_fail {
+
+    public static final int INF = (int) 1e9;
+
+    public static int n, m, start, end;
+
+    public static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+
+    public static int[] d;
+
+    static ArrayList<Integer> routes = new ArrayList<>();
+    static int[] route;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<Node>());
+        }
+
+        d = new int[n + 1];
+        route = new int[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int dest = Integer.parseInt(st.nextToken());
+            int dis = Integer.parseInt(st.nextToken());
+            graph.get(start).add(new Node(dest, dis));
+        }
+        st = new StringTokenizer(br.readLine());
+        start = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
+        Arrays.fill(d, INF);
+
+
+        dijkstra(start);
+
+        System.out.println(d[end]);
+        System.out.println(routes.size());
+        for (int i = 0; i < routes.size(); i++) {
+            System.out.print(routes.get(i) + " ");
+        }
+
+    }
+
+    private static void dijkstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
+        d[start] = 0;
+        pq.offer(new Node(start, 0));
+
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            int now = node.getIndex();
+            int dist = node.getDistance();
+
+            if (d[now] < dist) {
+                continue;
+            }
+
+            for (int i = 0; i < graph.get(now).size(); i++) {
+                int cost = graph.get(now).get(i).getDistance() + d[now];
+                if (cost < d[graph.get(now).get(i).getIndex()]) {
+                    d[graph.get(now).get(i).getIndex()] = cost;
+                    pq.offer(new Node(graph.get(now).get(i).getIndex(), cost));
+                    route[graph.get(now).get(i).getIndex()] = now;
+                }
+            }
+        }
+
+        int node = end;
+        while (node != 0) {
+            routes.add(node);
+            node = route[node];
+        }
+    }
+}
+
+class Node implements Comparable<Node> {
+
+    int index;
+    int distance;
+
+    public Node(int index, int distance) {
+        this.index = index;
+        this.distance = distance;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        if (this.distance < o.distance) {
+            return -1;
+        }
+        return 1;
+    }
+}
