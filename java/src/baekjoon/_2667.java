149fd8f361f139190da62197c093a92c24d6f339
yuhwanwoo <6514687@naver.com>
2021-11-27 16:31:19 +0900

    boj 2667 단지번호붙이기

--- java/src/baekjoon/_2667.java
+++ java/src/baekjoon/_2667.java
@@ -4,17 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class _2667 {
     public static int[][] graph;
-    public static int[][] visited;
+
+    public static int n;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
 
         graph = new int[n][n];
-        visited = new int[n][n];
 
         ArrayList<Integer> answer = new ArrayList<>();
 
@@ -28,18 +29,36 @@ public class _2667 {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (graph[i][j] == 1) {
-
+                    int cnt = dfs(i, j);
+                    answer.add(cnt);
                 }
             }
+        }
+
+        Collections.sort(answer);
+
+        System.out.println(answer.size());
+        for (int i = 0; i < answer.size(); i++) {
+            System.out.println(answer.get(i));
         }
     }
 
     public static int dfs(int x, int y) {
+        if (x < 0 || y < 0 || x >= n || y >= n) {
+            return 0;
+        }
+        if (graph[x][y] == 1) {
+            graph[x][y] = 0;
+            int cnt = 0;
+            cnt += 1;
+            cnt += dfs(x + 1, y);
+            cnt += dfs(x, y + 1);
+            cnt += dfs(x - 1, y);
+            cnt += dfs(x, y - 1);
+
+            return cnt;
+        }
 
         return 0;
     }
-
-
-
-
 }
