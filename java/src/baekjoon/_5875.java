6a3797a4410fc9e4a501c42091a3d803293e1d21
yuhwanwoo <6514687@naver.com>
2021-12-06 19:14:42 +0900

    boj 5875 오타 ~ing

---
+++ java/src/baekjoon/_5875.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _5875 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        int size = input.length();
+
+        int left_bracket = 0;
+        int right_bracket = 0;
+        int total_bracket = 0;
+        int result = 0;
+
+        String[] arr = input.split("");
+    }
+}
