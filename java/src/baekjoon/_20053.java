188eafd258a6ee230f1e21464ab5ff46e77c1dc4
yuhwanwoo <6514687@naver.com>
2021-12-11 23:56:38 +0900

    boj 20053 최소, 최대 2

--- java/src/baekjoon/_20053.java
+++ java/src/baekjoon/_20053.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class _20053 {
     public static void main(String[] args) throws IOException {
@@ -14,9 +15,17 @@ public class _20053 {
             PriorityQueue<Integer> minpq = new PriorityQueue<>();
             PriorityQueue<Integer> maxpq = new PriorityQueue<>();
             int n = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                
+                int num = Integer.parseInt(st.nextToken());
+                minpq.add(num);
+                maxpq.add(-num);
             }
+
+            int min = minpq.poll();
+            int max = -maxpq.poll();
+
+            System.out.println(min + " " + max);
         }
     }
 }
