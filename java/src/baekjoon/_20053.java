09db7ed5fb3c552849b035b89b042d7147e6d0da
yuhwanwoo <6514687@naver.com>
2021-12-11 23:48:17 +0900

    boj 20053 init

---
+++ java/src/baekjoon/_20053.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class _20053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            PriorityQueue<Integer> minpq = new PriorityQueue<>();
+            PriorityQueue<Integer> maxpq = new PriorityQueue<>();
+            int n = Integer.parseInt(br.readLine());
+            for (int j = 0; j < n; j++) {
+                
+            }
+        }
+    }
+}
