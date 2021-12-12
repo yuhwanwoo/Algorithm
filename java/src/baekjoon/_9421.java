e14448f2416b873b42b2699a86d2f84fa1ed43fd
yuhwanwoo <6514687@naver.com>
2021-12-12 22:35:45 +0900

    소수상근수 ~ing

--- java/src/baekjoon/_9421.java
+++ java/src/baekjoon/_9421.java
@@ -5,10 +5,28 @@ import java.util.*;
 
 
 public class _9421 {
+    static boolean[] judge = new boolean[1000001];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for (int i = 1; i < n; i++) {
+            if (PrimeNum(i)) {
+                answer.add(i);
+            }
+        }
+
+    }
+
+    public static boolean PrimeNum(int n) {
+        if (judge[n]) {
+            return true;
+        }
+
         
+
+        return false;
     }
 }
