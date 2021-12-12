d618382df843021843bb681b7eb61f57450ed76c
yuhwanwoo <6514687@naver.com>
2021-12-12 22:17:46 +0900

    9421 소수상근수 init

---
+++ java/src/baekjoon/_9421.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+
+public class _9421 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        
+    }
+}
