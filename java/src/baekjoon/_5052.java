78a996fa4c179372edcc5e00c6517fdb0fbafd2d
yuhwanwoo <6514687@naver.com>
2021-12-24 10:18:53 +0900

    boj 5052 init

---
+++ java/src/baekjoon/_5052.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _5052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+        }
+
+    }
+}
