51a7d97fc94ae63c0680ac7991ee0cd3ea15a540
yuhwanwoo <6514687@naver.com>
2022-01-09 00:33:57 +0900

    boj 11779 init

---
+++ java/src/baekjoon/_11779.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _11779 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < m; i++) {
+
+        }
+
+        int start = Integer.parseInt(br.readLine());
+        int end = Integer.parseInt(br.readLine());
+    }
+}
