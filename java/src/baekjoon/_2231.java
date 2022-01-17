4fed9d6a263ff1a535f931a6cbc6c9de2f3050a2
yuhwanwoo <6514687@naver.com>
2022-01-17 22:39:37 +0900

    boj 2231

---
+++ java/src/baekjoon/_2231.java
@@ -0,0 +1,41 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2231 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int answer = 1;
+
+        while (answer <= 1000000) {
+            int now = answer;
+
+            int seg_sum = 0;
+
+            while(now != 0) {
+                seg_sum += (now % 10);
+
+                now /= 10;
+            }
+
+            if (answer + seg_sum == n) {
+                break;
+            }
+
+
+            answer += 1;
+        }
+
+
+        if (answer == 1000001) {
+            System.out.println(0);
+        }else{
+            System.out.println(answer);
+        }
+    }
+}
