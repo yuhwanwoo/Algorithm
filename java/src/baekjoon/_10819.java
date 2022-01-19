2ca53ead6c714a2d1af2dc60144f5e8e35f495c0
yuhwanwoo <6514687@naver.com>
2022-01-19 21:57:41 +0900

    boj 10819 최대 간격

--- java/src/baekjoon/_10819.java
+++ java/src/baekjoon/_10819.java
@@ -6,20 +6,52 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class _10819 {
-    
+
     static int[] arr;
+    static boolean[] visited;
+    static int[] output;
+    static int  n;
+    static int answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         arr = new int[n];
+        visited = new boolean[n];
+        output = new int[n];
 
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        
-        // 백트레킹 쓰자
+
+        permutations(0);
+
+        System.out.println(answer);
+
+    }
+
+    public static void permutations(int depth) {
+        if (depth == n) {
+            int sum = 0;
+            for(int i = 1; i < n; i++) {
+                sum += Math.abs(output[i] - output[i - 1]);
+            }
+
+            answer = Math.max(answer, sum);
+            return;
+        }
+
+        for (int i=0; i<n; i++) {
+            if (visited[i] != true) {
+                visited[i] = true;
+                output[depth] = arr[i];
+                permutations(depth + 1);
+                output[depth] = 0; // 이 줄은 없어도 됨
+                visited[i] = false;;
+            }
+        }
+
     }
 }
