36fdd6a2e3b7dbbd3f3b0d3704278ceb41ad1ca2
yuhwanwoo <6514687@naver.com>
2022-01-21 18:14:37 +0900

    boj 18866 init

---
+++ java/src/baekjoon/_18866.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _18866 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+        }
+    }
+}
