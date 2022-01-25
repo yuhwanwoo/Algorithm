e8b63ec62fa125ed3d7302cc1f7e9c94f7f1e2a5
yuhwanwoo <6514687@naver.com>
2022-01-25 20:03:47 +0900

    18866 fail 0처리 어떻게...?

--- java/src/baekjoon/_18866.java
+++ java/src/baekjoon/_18866.java
@@ -15,25 +15,61 @@ public class _18866 {
         int[] happiness = new int[n];
         int[] fatigue = new int[n];
 
+        int Hmax = 0;
+        int Hmin = Integer.MAX_VALUE;
+        int Fmax = 0;
+        int Fmin = Integer.MAX_VALUE;
+
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             happiness[i] = u;
             fatigue[i] = v;
+            Hmax = Math.max(Hmax, u);
+            Hmin = Math.min(Hmin, u);
+            Fmax = Math.max(Fmax, v);
+            Fmin = Math.min(Fmin, v);
         }
 
-        int hs = 0;
-        int he = n - 1;
-        int fs = 0;
-        int fe = n - 1;
+        int[] MinHappiness = new int[n];
+        int[] MaxHappiness = new int[n];
+        int[] MinFatigue = new int[n];
+        int[] MaxFatigue = new int[n];
 
-        int hmax = 0;
-        int hmin = Integer.MAX_VALUE;
+        MinHappiness[0] = happiness[0];
+        MaxHappiness[n - 1] = happiness[n - 1];
+        MaxFatigue[0] = fatigue[0];
+        MinFatigue[n - 1] = fatigue[n - 1];
 
-        while (true) {
+        // 0 처리 어떻게..?
 
+
+        for(int i = 1; i < n; i++) {
+            MinHappiness[i] = Math.min(happiness[i - 1], happiness[i]);
+            MaxHappiness[n - 1 - i] = Math.max(happiness[n - i], happiness[n - 1 - i]);
+            MaxFatigue[i] = Math.max(fatigue[i - 1], fatigue[i]);
+            MinFatigue[n - 1 - i] = Math.min(fatigue[n - i], fatigue[n - 1 - i]);
         }
+
+        int vday = 0;
+        int uday = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (MaxFatigue[i] > MinFatigue[i]) {
+                uday = i;
+                break;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (MinHappiness[i] < MaxHappiness[i]) {
+                vday = i;
+                break;
+            }
+        }
+
+        System.out.println(vday + " " + uday);
 
 
     }
