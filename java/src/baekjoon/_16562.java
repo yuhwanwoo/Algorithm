e98c27305ad2b34735d7c7a9db8c4b8917e7c4d6
yuhwanwoo <6514687@naver.com>
2022-02-01 11:32:49 +0900

    boj 16562 init

---
+++ java/src/baekjoon/_16562.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _16562 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        
+        // 유니온 파인드 써보자
+
+
+    }
+}
