37b6b6eb55ba6b6f7785d42a77554ca3d6e4409c
yuhwanwoo <6514687@naver.com>
2022-02-07 00:02:44 +0900

    boj 3673 init

---
+++ java/src/baekjoon/_3673.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+// 누적합 예상
+public class _3673 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int c = Integer.parseInt(br.readLine());
+
+        while (c != 0) {
+
+
+
+            c--;
+        }
+    }
+}
