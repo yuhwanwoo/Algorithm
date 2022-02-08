6df1e377257124aa7f3f172a88ac8e8fada5925e
yuhwanwoo <6514687@naver.com>
2022-02-08 22:24:16 +0900

    boj 3673 ~ing

--- java/src/baekjoon/_3673.java
+++ java/src/baekjoon/_3673.java
@@ -3,6 +3,7 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 
 // 누적합 예상
@@ -11,8 +12,18 @@ public class _3673 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int c = Integer.parseInt(br.readLine());
 
+        StringTokenizer st;
         while (c != 0) {
+            st = new StringTokenizer(br.readLine());
+            int d = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
 
+            int[] arr = new int[n];
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
 
 
             c--;
