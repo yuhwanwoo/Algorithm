e3bc58aba00aa6f79e8fed3fa7d2a055a0c12024
yuhwanwoo <6514687@naver.com>
2022-02-18 18:12:37 +0900

    boj 1633 init

---
+++ java/src/baekjoon/_1633.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class _1633 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
