b05c609c3f635f88e4928bac724125b41dbaf8fa
yuhwanwoo <6514687@naver.com>
2022-02-26 00:29:13 +0900

    boj 6118 완료

--- java/src/baekjoon/_2164.java
+++ java/src/baekjoon/_2164.java
@@ -3,22 +3,33 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class _2164 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        ArrayList<Integer> arr = new ArrayList<>();
+        Queue<Integer> arr = new LinkedList<>();
 
         for (int i = 1; i < n + 1; i++) {
             arr.add(i);
         }
-        
 
-        while (true) {
+        boolean flag = true;
 
+        while (arr.size() != 1) {
+            if (flag) {
+                arr.poll();
+                flag = false;
+            } else {
+                int firstElement = arr.poll();
+                arr.add(firstElement);
+                flag = true;
+            }
         }
+
+        System.out.println(arr.poll());
     }
 }
