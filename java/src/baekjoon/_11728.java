0873d9e58c8c1577a423fbc2106a6b669b6a6dad
yuhwanwoo <6514687@naver.com>
2022-04-22 18:52:07 +0900

    boj 11728

---
+++ java/src/baekjoon/_11728.java
@@ -0,0 +1,37 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class _11728 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            pq.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            pq.add(Integer.parseInt(st.nextToken()));
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < (n + m); i++) {
+            int now = pq.poll();
+            sb.append(now + " ");
+        }
+
+        System.out.println(sb);
+    }
+}
