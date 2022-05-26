76c4771b3ee1163773a7a6eed262fbfdc3cc482e
yuhwanwoo <6514687@naver.com>
2022-05-26 23:28:22 +0900

    'boj_1421_2428_fail'

--- java/src/baekjoon/_1421.java
+++ java/src/baekjoon/_1421.java
@@ -5,14 +5,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class _2428 {
+public class _1421 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Integer n = Integer.parseInt(br.readLine());
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int w = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < n; i++) {
-            int input = Integer.parseInt(st.nextToken());
+
         }
+
     }
 }
