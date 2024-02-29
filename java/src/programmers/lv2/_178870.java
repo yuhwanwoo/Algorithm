42e3797bb2419f16c3ba21c24e8d595f493253ca
yuhwanwoo <6514687@naver.com>
2024-02-29 18:38:15 +0900

    [programmers] 178870 init

---
+++ java/src/programmers/lv2/_178870.java
@@ -0,0 +1,17 @@
+package programmers.lv2;
+
+public class _178870 {
+    public static void main(String[] args) {
+        int[] sequence = {1, 2, 3, 4, 5};
+        int k = 7;
+        solution(sequence, k);
+    }
+
+    public static int[] solution(int[] sequence, int k) {
+        int[] answer = {};
+        int start = 0;
+        int end = 0;
+
+        return answer;
+    }
+}
