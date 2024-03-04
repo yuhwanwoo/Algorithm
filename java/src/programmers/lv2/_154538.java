db60162b37fda58e693e21dc3dfd1961b984407b
yuhwanwoo <6514687@naver.com>
2024-03-04 20:49:30 +0900

    [programmers] 154538 init

---
+++ java/src/programmers/lv2/_154538.java
@@ -0,0 +1,12 @@
+package programmers.lv2;
+
+public class _154538 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution(int x, int y, int n) {
+        int answer = 0;
+        return answer;
+    }
+}
