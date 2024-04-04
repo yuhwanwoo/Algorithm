f9b1493ba55c18623c7d189f6c812425acc380f6
yuhwanwoo <6514687@naver.com>
2024-04-05 07:47:50 +0900

    [programmers] 148653

--- java/src/programmers/lv2/_148653.java
+++ java/src/programmers/lv2/_148653.java
@@ -2,30 +2,28 @@ package programmers.lv2;
 
 public class _148653 {
     public static void main(String[] args) {
-//        solution(16); // 6
-//        solution(2554); //16
+        solution(16); // 6
+        solution(2554); //16
 //        solution(1);
 //        solution(9);
 //        solution(555);
-//        solution(95); // 6
-//        solution(45); //9
-//        solution(678); // 8
-//        solution(999); //2
-//        solution(155); //11
-//        solution(154); //10
-//        solution(545); //14
+        solution(95); // 6
+        solution(45); //9
+        solution(678); // 8
+        solution(999); //2
+        solution(155); //11
+        solution(154); //10
+        solution(545); //14
 //        solution(5555);
 //        solution(55);
 //        solution(5);
-        solution(56);
+//        solution(56); // 9
     }
 
     public static int solution(int storey) {
         int answer = 0;
         int mod;
         while (storey != 0) {
-            System.out.println("=================");
-            System.out.println("storey = " + storey);
             if (storey % 10 > 5) {
                 answer += (10 - (storey % 10));
                 mod = 1;
@@ -41,12 +39,10 @@ public class _148653 {
                 answer += (storey % 10);
                 mod = 0;
             }
-            System.out.println("mod = " + mod);
-            System.out.println("storey after = " + storey);
             storey /= 10;
             storey += mod;
         }
-        System.out.println("answer = " + answer);
+
         return answer;
     }
 }
