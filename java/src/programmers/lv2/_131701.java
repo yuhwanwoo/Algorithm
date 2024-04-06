637cc422c173b5ab3e897369064914447a57b9e8
yuhwanwoo <6514687@naver.com>
2024-04-06 23:10:30 +0900

    [programmers] 131701

--- java/src/programmers/lv2/_131701.java
+++ java/src/programmers/lv2/_131701.java
@@ -1,11 +1,27 @@
 package programmers.lv2;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class _131701 {
     public static void main(String[] args) {
         solution(new int[]{7, 9, 1, 1, 4});
     }
 
     private static int solution(int[] elements) {
-        return 0;
+        HashSet<Integer> set = new HashSet<>();
+        ArrayList<Integer> array = new ArrayList<>();
+        for (int i = 0; i < elements.length * 2 - 1; i++) {
+            array.add(elements[i % elements.length]);
+        }
+        int sum;
+        for (int i = 0; i < elements.length; i++) {
+            sum = 0;
+            for (int j = i; j < i + elements.length; j++) {
+                sum += array.get(j);
+                set.add(sum);
+            }
+        }
+        return set.size();
     }
 }
