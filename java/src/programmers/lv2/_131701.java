441257dfb9011d46c5cb19e1fc42cd19c4802b45
yuhwanwoo <6514687@naver.com>
2024-04-06 22:19:00 +0900

    [programmers] 131701 init

---
+++ java/src/programmers/lv2/_131701.java
@@ -0,0 +1,11 @@
+package programmers.lv2;
+
+public class _131701 {
+    public static void main(String[] args) {
+        solution(new int[]{7, 9, 1, 1, 4});
+    }
+
+    private static int solution(int[] elements) {
+        return 0;
+    }
+}
