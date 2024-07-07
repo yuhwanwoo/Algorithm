e58a3ad93abe5d8031a8fdd74f0492ee0ab8022b
yuhwanwoo <6514687@naver.com>
2024-07-07 20:42:03 +0900

    87390 ans

---
+++ java/src/programmers/lv2/_87390.java
@@ -0,0 +1,13 @@
+package programmers.lv2;
+
+public class _87390 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] solution(int n, long left, long right) {
+        int size = (int) (right - left + 1);
+        int[] answer = new int[size];
+        return answer;
+    }
+}
