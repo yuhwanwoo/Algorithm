e5379a4bd1389257f673d4b8d01906be7cbce100
yuhwanwoo <6514687@naver.com>
2024-07-09 19:42:05 +0900

    programmers 87390

--- java/src/programmers/lv2/_87390.java
+++ java/src/programmers/lv2/_87390.java
@@ -2,12 +2,21 @@ package programmers.lv2;
 
 public class _87390 {
     public static void main(String[] args) {
-
+        solution(3, 2, 5);
     }
 
     public static int[] solution(int n, long left, long right) {
         int size = (int) (right - left + 1);
         int[] answer = new int[size];
+        int idx = 0;
+
+        for (long i = left; i <= right; i++) {
+            long row = i / n;
+            long col = i % n;
+
+            answer[idx++] = (int) Math.max(row, col) + 1;
+        }
+
         return answer;
     }
 }
