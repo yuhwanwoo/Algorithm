7a16cc2745eef80f3917aaa3ca13de290ce1dead
yuhwanwoo <6514687@naver.com>
2024-07-26 17:29:50 +0900

    programmers 131127

--- java/src/programmers/lv2/_87946.java
+++ java/src/programmers/lv2/_87946.java
@@ -1,22 +1,32 @@
 package programmers.lv2;
 
 public class _87946 {
+    private static boolean[] visited;
+    private static int answer = -1;
+
     public static void main(String[] args) {
         int k = 80;
         int[][] dungeons = {{80, 20}, {50, 40}, {30, 10}};
     }
 
     public static int solution(int k, int[][] dungeons) {
-        int answer = -1;
+
+        visited = new boolean[dungeons.length];
+        dfs(k, dungeons, 0);
 
         return answer;
     }
 
-    private static int dfs(int depth, int[][] dungeons, int count) {
-        if (depth == dungeons.length) {
-            return count;
+    private static void dfs(int k, int[][] dungeons, int count) {
+        answer = Math.max(answer, count);
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && dungeons[i][0] <= k) {
+                visited[i] = true;
+                k -= dungeons[i][1];
+                dfs(k, dungeons, count + 1);
+                visited[i] = false;
+                k += dungeons[i][1];
+            }
         }
-
-        return dfs(depth + 1, dungeons, count);
     }
 }
