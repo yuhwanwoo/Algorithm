a191364246cb0874090947764ba132c066de7d86
yuhwanwoo <6514687@naver.com>
2024-10-26 18:26:02 +0900

    programmers 132267

--- java/src/programmers/lv1/_132267.java
+++ java/src/programmers/lv1/_132267.java
@@ -2,6 +2,19 @@ package programmers.lv1;
 
 public class _132267 {
     public static void main(String[] args) {
-
+        solution(2, 1, 20);
+        solution(3, 1, 20);
     }
+
+    public static int solution(int a, int b, int n) {
+        int answer = 0;
+
+        while (n >= a) {
+            answer += ((n / a) * b);
+            n = (n % a) + ((n / a) * b);
+        }
+
+        return answer;
+    }
+
 }
