f0787343cecf0737b5db18c6a59e8a4fb46c2fd9
yuhwanwoo <6514687@naver.com>
2025-01-24 17:08:36 +0900

    programmers 140107 init

---
+++ java/src/programmers/lv2/_140107.java
@@ -0,0 +1,13 @@
+package programmers.lv2;
+
+public class _140107 {
+    public static void main(String[] args) {
+        solution(2, 4);
+        solution(1, 5);
+    }
+
+    public static long solution(int k, int d) {
+        long answer = 0;
+        return answer;
+    }
+}
