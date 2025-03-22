11da81e09898403cbb884f5060d4d86c52ce02e5
yuhwanwoo <6514687@naver.com>
2025-03-22 21:01:27 +0900

    programmers 140107

--- java/src/programmers/lv2/_140107_ans.java
+++ java/src/programmers/lv2/_140107_ans.java
@@ -1,6 +1,6 @@
 package programmers.lv2;
 
-public class _140107 {
+public class _140107_ans {
     public static void main(String[] args) {
         solution(2, 4);
         solution(1, 5);
@@ -8,9 +8,12 @@ public class _140107 {
 
     public static long solution(int k, int d) {
         long answer = 0;
-        for (int i = 0; i <= d; i += k) {
-            answer += (long) (Math.sqrt((long) d * d - (long) i * i) / k + 1);
+
+        for (int x = 0; x <= d; x += k) {
+            int y = (int) Math.sqrt((long) d * d - (long) x * x);
+            answer += (y / k + 1);
         }
+
         return answer;
     }
 }
