1953a54fe2d7f8ae567ec8e133d4600e7d786ef9
yuhwanwoo <6514687@naver.com>
2025-03-26 21:33:41 +0900

    programmers 169198 init

---
+++ java/src/programmers/lv2/_169198.java
@@ -0,0 +1,12 @@
+package programmers.lv2;
+
+public class _169198 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] solution(int m, int n, int startX, int startY, int[][] balls) {
+        int[] answer = {};
+        return answer;
+    }
+}
