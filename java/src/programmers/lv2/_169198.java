d3258346c729ad4704f3a5d7f8396531c7718d46
yuhwanwoo <6514687@naver.com>
2025-03-28 21:50:28 +0900

    위의 벽의 경우

--- java/src/programmers/lv2/_169198.java
+++ java/src/programmers/lv2/_169198.java
@@ -11,26 +11,34 @@ public class _169198 {
      */
     public static int[] solution(int m, int n, int startX, int startY, int[][] balls) {
         int[] answer = new int[balls.length];
+        int t = 0;
         for (int i = 0; i < balls.length; i++) {
             int min = Integer.MAX_VALUE;
             int x = balls[i][0];
             int y = balls[i][1];
-            if (y == startY && startX > x) {
-                int t = (x*startY + y* startX)/(startX+x);
-                System.out.println("t = " + t);
-                min = Math.min(startX * startX + (startY - t) * (startY - t), min);
-            }
-
-
-
             // y가 같으면서 x가 startX보다 크면안됨
             /*
             (startY-t)x = startX(t-y)
             x*startY-tx = t*startX-y*startX
             t(startX+x) = x*startY + y * startX
             t = (x*startY + y* startX)/(startX+x)
+             */
+            if (!(y == startY && startX > x)) {
+                t = (x * startY + y * startX) / (startX + x);
+                min = Math.min(startX * startX + (startY - t) * (startY - t), min);
+            }
+            /*
+            (t, n)
+            (t-startX)/(n-startY) = (x-t)/(n-y)
+            (t-startX)*(n-y) = (x-t)(n-startY)
 
              */
+            if (!(x == startX && startY > y)) {
+//                int t = t
+            }
+
+
+            answer[i] = min;
         }
         return answer;
     }
