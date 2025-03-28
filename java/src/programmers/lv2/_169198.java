d0aebe54d0de6dc3bb34882cd0cdc6f90bc29270
yuhwanwoo <6514687@naver.com>
2025-03-28 12:58:02 +0900

    케이스 추가

--- java/src/programmers/lv2/_169198.java
+++ java/src/programmers/lv2/_169198.java
@@ -5,8 +5,18 @@ public class _169198 {
 
     }
 
+    /*
+    가로m
+    세로n
+     */
     public static int[] solution(int m, int n, int startX, int startY, int[][] balls) {
         int[] answer = {};
+        for (int[] ball : balls) {
+            int min = 0;
+            int x = ball[0];
+            int y = ball[1];
+            // y가 같으면서 x가 startX보다 크면안됨
+        }
         return answer;
     }
 }
