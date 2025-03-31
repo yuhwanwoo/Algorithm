1a6c8801bde72271bf1cb5cca09af68c601cf977
yuhwanwoo <6514687@naver.com>
2025-03-31 12:33:04 +0900

    [programmers] 169198

--- java/src/programmers/lv2/_169198.java
+++ java/src/programmers/lv2/_169198.java
@@ -2,7 +2,11 @@ package programmers.lv2;
 
 public class _169198 {
     public static void main(String[] args) {
-        solution(10, 10, 3, 7, new int[][]{{7, 7}, {2, 7}, {7, 3}});
+        solution(10, 10, 3, 7, new int[][]{
+                {7, 7},
+                {2, 7},
+                {7, 3}
+        });
     }
 
     /*
@@ -11,37 +15,30 @@ public class _169198 {
      */
     public static int[] solution(int m, int n, int startX, int startY, int[][] balls) {
         int[] answer = new int[balls.length];
-        int t = 0;
         for (int i = 0; i < balls.length; i++) {
             int min = Integer.MAX_VALUE;
             int x = balls[i][0];
             int y = balls[i][1];
-            //todo min 계산 식 변경해야함
 
-            // y가 같으면서 x가 startX보다 크면안됨
-            /*
-            (startY-t)x = startX(t-y)
-            x*startY-tx = t*startX-y*startX
-            t(startX+x) = x*startY + y * startX
-            t = (x*startY + y* startX)/(startX+x)
-             */
+            // 왼
             if (!(y == startY && startX > x)) {
-                t = (x * startY + y * startX) / (startX + x);
-                min = Math.min(startX * startX + (startY - t) * (startY - t), min);
+                min = (int) Math.min(Math.pow(startX + x, 2) + Math.pow(startY - y, 2), min);
             }
-            /*
-            (t, n)
-            (t-startX)/(n-startY) = (x-t)/(n-y)
-            (t-startX)*(n-y) = (x-t)(n-startY)
 
-             */
+            //위
+            if (!(x == startX && startY < y)) {
+                min = (int) Math.min(Math.pow(n - startY + n - y, 2) + Math.pow(x - startX, 2), min);
+            }
 
+            // 오
+            if (!(y == startY && startX < x)) {
+                min = (int) Math.min(Math.pow(m - x + m - startX, 2) + Math.pow(y - startY, 2), min);
+            }
+            // 밑
             if (!(x == startX && startY > y)) {
-                t = (n * x - x * startY + n * startX - y * startX) / (2 * n - y - startY);
-//                min = Math.min((t- startX))
+                min = (int) Math.min(Math.pow(y + startY, 2) + Math.pow(x - startX, 2), min);
             }
-
-
+            System.out.println("min = " + min);
             answer[i] = min;
         }
         return answer;
