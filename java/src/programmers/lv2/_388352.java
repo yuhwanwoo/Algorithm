d0984cb15a7cce6563468333b321831876484073
yuhwanwoo <6514687@naver.com>
2025-04-01 17:28:21 +0900

    388352 init

---
+++ java/src/programmers/lv2/_388352.java
@@ -0,0 +1,14 @@
+package programmers.lv2;
+
+public class _388352 {
+    public static void main(String[] args) {
+        int n = 10;
+        int[][] q = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {3, 7, 8, 9, 10}, {2, 5, 7, 9, 10}, {3, 4, 5, 6, 7}};
+        int[] ans = {2, 3, 4, 3, 3};
+        solution(n, q, ans);
+    }
+    public static int solution(int n, int[][] q, int[] ans) {
+        int answer = 0;
+        return answer;
+    }
+}
