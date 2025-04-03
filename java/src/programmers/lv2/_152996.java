3fb6a3cca2848d717fe2756c227f77bfd9301137
yuhwanwoo <6514687@naver.com>
2025-04-03 15:46:00 +0900

    152996 init

---
+++ java/src/programmers/lv2/_152996.java
@@ -0,0 +1,13 @@
+package programmers.lv2;
+
+public class _152996 {
+    public static void main(String[] args) {
+        int[] weights = {100, 180, 360, 100, 270};
+        solution(weights);
+    }
+
+    public static long solution(int[] weights) {
+        long answer = 0;
+        return answer;
+    }
+}
