9305527eabfc7996f90d7be031e3544be1877de0
yuhwanwoo <6514687@naver.com>
2025-04-03 15:48:47 +0900

    map 적용

--- java/src/programmers/lv2/_152996.java
+++ java/src/programmers/lv2/_152996.java
@@ -1,5 +1,7 @@
 package programmers.lv2;
 
+import java.util.HashMap;
+
 public class _152996 {
     public static void main(String[] args) {
         int[] weights = {100, 180, 360, 100, 270};
@@ -7,7 +9,13 @@ public class _152996 {
     }
 
     public static long solution(int[] weights) {
+        HashMap<Integer, Integer> count = new HashMap<>();
+
+        for (int weight : weights) {
+            count.put(weight, count.getOrDefault(weight, 0) + 1);
+        }
         long answer = 0;
+
         return answer;
     }
 }
