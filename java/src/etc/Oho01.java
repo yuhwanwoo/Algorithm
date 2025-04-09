d289c9d71a7f7e7097b934e7be379d965664ee33
yuhwanwoo <6514687@naver.com>
2025-04-09 18:58:19 +0900

    oh failed

---
+++ java/src/etc/Oho01.java
@@ -0,0 +1,18 @@
+package etc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Oho01 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 1, 1, 3, 1, 2};
+        int[][] target = {{1, 2}, {2, 3}, {1, 3}};
+
+    }
+
+    private static List<Integer> solution(int[] arr, int[][] target) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        return answer;
+    }
+}
