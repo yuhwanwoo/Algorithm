15cbadf8ef5be7a5f09a9684f076ef9fa061486a
yuhwanwoo <6514687@naver.com>
2025-04-11 23:51:58 +0900

    87946 init

---
+++ java/src/programmers/lv2/_87946_2.java
@@ -0,0 +1,13 @@
+package programmers.lv2;
+
+public class _87946_2 {
+    public static void main(String[] args) {
+        int k = 80;
+        int[][] dungeons = {{80, 20}, {50, 40}, {30, 10}};
+        solution(k, dungeons);
+    }
+
+    public static int solution(int k, int[][] dungeons) {
+        return 0;
+    }
+}
