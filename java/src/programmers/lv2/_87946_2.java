40316bf34b9d44bd4779f99093d4c4b0f324e4c9
yuhwanwoo <6514687@naver.com>
2025-04-12 10:49:21 +0900

    [programmers] 87946

--- java/src/programmers/lv2/_87946_2.java
+++ java/src/programmers/lv2/_87946_2.java
@@ -1,6 +1,8 @@
 package programmers.lv2;
 
 public class _87946_2 {
+    private static boolean[] visited;
+    private static int answer;
     public static void main(String[] args) {
         int k = 80;
         int[][] dungeons = {{80, 20}, {50, 40}, {30, 10}};
@@ -8,6 +10,19 @@ public class _87946_2 {
     }
 
     public static int solution(int k, int[][] dungeons) {
-        return 0;
+        visited = new boolean[dungeons.length];
+        dfs(dungeons, k, 0);
+        return answer;
+    }
+
+    private static void dfs(int[][] dungeons, int restEnergy, int count) {
+        answer = Math.max(answer, count);
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && dungeons[i][0] <= restEnergy) {
+                visited[i] = true;
+                dfs(dungeons, restEnergy - dungeons[i][1], count + 1);
+                visited[i] = false;
+            }
+        }
     }
 }
