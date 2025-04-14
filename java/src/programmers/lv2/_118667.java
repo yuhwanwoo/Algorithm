0c4905f753fbd1acd90820ff0dc43ec9a3e5491c
yuhwanwoo <6514687@naver.com>
2025-04-14 18:05:58 +0900

    118667 init

---
+++ java/src/programmers/lv2/_118667.java
@@ -0,0 +1,14 @@
+package programmers.lv2;
+
+public class _118667 {
+    public static void main(String[] args) {
+        int[] queue1 = {3,2,7,2};
+        int[] queue2 = {4,6,5,1};
+        solution(queue1, queue2);
+    }
+
+    public static int solution(int[] queue1, int[] queue2) {
+        int answer = -2;
+        return answer;
+    }
+}
