0112df5196b7d93197d34a3fd0457ca28b8f11e2
yuhwanwoo <6514687@naver.com>
2025-04-20 17:59:22 +0900

    12923 ans

---
+++ java/src/programmers/lv2/_12923_ans.java
@@ -0,0 +1,48 @@
+package programmers.lv2;
+
+public class _12923_ans {
+    public int[] solution(long begin, long end) {
+        int[] result = new int[(int) (end - begin + 1)];
+        int index = 0;
+        for (int i = (int) begin; i <= end; i++, index++) {
+            if (isPrime(i)) {
+                result[index] = 1;
+            } else {
+                result[index] = getMaxDivisor(i);
+            }
+        }
+
+        return result;
+    }
+
+    public boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private int getMaxDivisor(int number) {
+        if (number == 1) {
+            return 0;
+        }
+
+        int result = 1;
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0 && number / i <= 10000000) {
+                result = number / i;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
