a4a4517bae21f664b6d25d67e5cb78d39378e8e3
yuhwanwoo <6514687@naver.com>
2025-04-22 20:58:01 +0900

    131704 init

---
+++ java/src/programmers/lv2/_131704.java
@@ -0,0 +1,13 @@
+package programmers.lv2;
+
+public class _131704 {
+    public static void main(String[] args) {
+        int[] order = {4, 3, 1, 2, 5};
+        solution(order);
+    }
+
+    public static int solution(int[] order) {
+        int answer = 0;
+        return answer;
+    }
+}
