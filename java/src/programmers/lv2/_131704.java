cbc173b705ce10fef597c404cb548902d55babaf
yuhwanwoo <6514687@naver.com>
2025-04-26 22:45:05 +0900

    131704 ans

--- java/src/programmers/lv2/_131704.java
+++ java/src/programmers/lv2/_131704.java
@@ -1,5 +1,7 @@
 package programmers.lv2;
 
+import java.util.Stack;
+
 public class _131704 {
     public static void main(String[] args) {
         int[] order = {4, 3, 1, 2, 5};
@@ -8,6 +10,24 @@ public class _131704 {
 
     public static int solution(int[] order) {
         int answer = 0;
+        int index = 0;
+
+        Stack<Integer> belt = new Stack<>();
+
+        for(int i=1;i<=order.length;i++){
+            belt.push(i);
+
+            while(!belt.isEmpty()){
+                if(belt.peek()==order[index]){
+                    belt.pop();
+                    index++;
+                    answer++;
+                }
+                else {
+                    break;
+                }
+            }
+        }
         return answer;
     }
 }
