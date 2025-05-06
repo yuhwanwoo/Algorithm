4316322739696c201abc663ea7ef95607d4cdc57
yuhwanwoo <6514687@naver.com>
2025-05-06 20:23:40 +0900

    StringToInteger fail

---
+++ java/src/leetcode/StringToIntegerAtoi.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class StringToIntegerAtoi {
+    //https://leetcode.com/problems/string-to-integer-atoi/
+    public static void main(String[] args) {
+
+    }
+
+    public static int myAtoi(String s) {
+        return 0;
+    }
+}
