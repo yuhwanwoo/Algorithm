5220d70a595fb0f620eb63b1cd3d510e6436a0ab
yuhwanwoo <6514687@naver.com>
2025-05-18 20:44:57 +0900

    slowly ans

--- java/src/leetcode/FourSum.java
+++ java/src/leetcode/FourSum.java
@@ -1,11 +1,13 @@
 package leetcode;
 
+import java.util.List;
+
 public class FourSum {
     public static void main(String[] args) {
 
     }
 
     public List<List<Integer>> fourSum(int[] nums, int target) {
-
+        return null;
     }
 }
