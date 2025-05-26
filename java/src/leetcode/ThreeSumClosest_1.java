f2e169d01af0b6e44863444f0f6091e8fac70b19
yuhwanwoo <6514687@naver.com>
2025-05-26 15:46:59 +0900

    [leetcode] threeSumClosest faster

--- java/src/leetcode/ThreeSumClosest_1.java
+++ java/src/leetcode/ThreeSumClosest_1.java
@@ -13,11 +13,24 @@ public class ThreeSumClosest_1 {
 
     public static int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
+        int closestSum = nums[0] + nums[1] + nums[2];
         for (int i = 0; i < nums.length - 2; i++) {
-            
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                    closestSum = sum;
+                }
+                if (sum == target) {
+                    return target;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
         }
-
-
-        return 0;
+        return closestSum;
     }
 }
