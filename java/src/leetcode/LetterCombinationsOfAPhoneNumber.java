9f23d34a11decfc849910a0c527c19df528a14df
yuhwanwoo <6514687@naver.com>
2025-06-06 01:19:15 +0900

    LetterCombinationsOfAPhoneNumber init

---
+++ java/src/leetcode/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.util.List;
+
+public class LetterCombinationsOfAPhoneNumber {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<String> letterCombinations(String digits) {
+        return List.of();
+    }
+
+
+
+}
