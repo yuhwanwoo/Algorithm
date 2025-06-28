6eaecec94bc6d742084a56427f56d3d64c6ba9f6
yuhwanwoo <6514687@naver.com>
2025-06-28 11:12:56 +0900

    [hkr] OrganizingContainersOfBalls fail

--- java/src/hackerrank/OrganizingContainersOfBalls.java
+++ java/src/hackerrank/OrganizingContainersOfBalls.java
@@ -19,28 +19,6 @@ public class OrganizingContainersOfBalls {
         1 1
 
          */
-
-        Map<Integer, Integer> countOfBallForType = new HashMap<>();
-        List<Integer> countOfBallInContainer = new ArrayList<>();
-
-        for (List<Integer> c : container) {
-            countOfBallInContainer.add(c.stream().reduce((i, j) -> i + j).get()); // 각 컨테이너에 저장된 볼의 총 갯수
-            IntStream.range(0, c.size()) // 볼 타입 별 갯수
-                    .forEach(i -> countOfBallForType.put(i, countOfBallForType.getOrDefault(i, 0) + c.get(i)));
-        }
-
-
-        for (int ballCount : countOfBallInContainer) {
-            Iterator<Integer> iterator = countOfBallForType.keySet().iterator();
-            while (iterator.hasNext()) {
-                int key = iterator.next();
-                if (countOfBallForType.get(key) == ballCount) {
-                    countOfBallForType.remove(key);
-                    break;
-                }
-            }
-        }
-
-        return countOfBallForType.isEmpty() ?  "Possible" : "Impossible";
+return "Impossible";
     }
 }
