5b0a31f8aa4507a8d598d640cf84331598098d36
yuhwanwoo <6514687@naver.com>
2025-07-05 19:14:51 +0900

    [hkr] biggerIsGreater_ans

---
+++ java/src/hackerrank/biggerIsGreater_ans.java
@@ -0,0 +1,43 @@
+package hackerrank;
+
+import java.util.Arrays;
+
+public class biggerIsGreater_ans {
+    public static void main(String[] args) {
+
+    }
+
+    public static String solve(String input) {
+        char[] chars = input.toCharArray();
+        int lastIdx = chars.length - 1;
+        int minVal = Integer.MAX_VALUE;
+        int minIdx = Integer.MAX_VALUE;
+        int turningPoint = 0;
+        String result = input;
+        for (int i = lastIdx; i > 0; i--) {
+            /** 작아지는 구간을 찾는다. */
+            if (chars[i] > chars[i - 1]) {
+                turningPoint = i - 1;
+                for (int k = i; k <= lastIdx; k++) {
+                    if (chars[turningPoint] < chars[k] && minVal > chars[k]) {
+                        minVal = chars[k];
+                        minIdx = k;
+                    }
+                }
+                swap(chars, turningPoint, minIdx);
+                Arrays.sort(chars, turningPoint + 1, lastIdx + 1);
+                result = new String(chars);
+                break;
+            }
+
+        }
+
+        return input.equals(result) ? "no answer" : result;
+    }
+
+    private static void swap(char[] chars, int i, int j) {
+        char tmp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = tmp;
+    }
+}
