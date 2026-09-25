61470c60d3952ca5291fa6141534b75dd8e04eb4
yuhwanwoo <6514687@naver.com>
2026-09-25 20:22:58 +0900

    동영상 재생기 init

---
+++ java/src/programmers/year2026/v1/_340213.java
@@ -0,0 +1,18 @@
+package programmers.year2026.v1;
+
+public class _340213 {
+
+    public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
+        String answer = "";
+        for (String command : commands) {
+            if (command.equals("prev")) {
+
+            }
+            if (command.equals("next")) {
+
+            }
+        }
+        return answer;
+    }
+
+}
